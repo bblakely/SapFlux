fcc665abbd10e8087de325aede44a053576283e5
bblakely <bblakely@nd.edu>
2018-06-28 12:36:52 -0500

    Finished prepare_treedata, including rearrangement (12 and 18 swapped) of data files and updated radial profile calculations (off-repository).

--- Prepare_treedata.R
+++ Prepare_treedata.R
@@ -24,6 +24,9 @@ syv.tree$SWA_calc[syv.tree$SPP=='OSVI']<-0.312*syv.tree$DBH[syv.tree$SPP=='OSVI'
 syv.tree$SWA_calc[syv.tree$SPP=='TIAM']<-0.77*syv.tree$DBH[syv.tree$SPP=='TIAM']^1.868 #Aspen b/c no basswood found yet; originally used Bond-Lamberty aspen w/ 35 year old stand age: 10^(2.415*log10(syv.tree$DBH[syv.tree$SPP=='TIAM'])-(0.033*35))
 syv.tree$SWA_calc[syv.tree$SPP=='FRPE']<-3.24*syv.tree$DBH[syv.tree$SPP=='FRPE']- 10.24#Oak, b/c ring porous. Originally used red maple: 0.83*syv.tree$DBH[syv.tree$SPP=='FRPE']^1.97
 
+#patch up SWA>BA problems
+syv.tree$SWA_calc[syv.tree$SWA_calc>syv.tree$BA]<-syv.tree$BA[syv.tree$SWA_calc>syv.tree$BA]
+
 #Back-calculating Sapwood depth (for radial profiles)
 wcr.tree$SWD_calc<-(wcr.tree$DBH/2)-(sqrt((wcr.tree$BA-wcr.tree$SWA)/3.14159))
 syv.tree$SWD_calc<-(syv.tree$DBH/2)-(sqrt((syv.tree$BA-syv.tree$SWA)/3.14159))
@@ -52,7 +55,7 @@ syv.tree$SWA_calc[syv.tree$ID==40]<-(syv.tree$BA[syv.tree$ID==40])-(3.14159*((sy
 syv.tree$SWA_meas<-NA;syv.tree$meas<-'N' #Make columns for 'measured' SWA (from SWD) and a flag for whether this has a measured value or not
 wcr.tree$SWA_meas<-NA;wcr.tree$meas<-'N'
 
-calcSWA<-function(dat, bark=0.8){
+calcSWA<-function(dat, bark=0){
   for (t in 1:nrow(dat)){
     if(!is.na(dat$SWD_meas[t])){  #If you have a measured value...
       
@@ -69,19 +72,18 @@ calcSWA<-function(dat, bark=0.8){
   return(dat)
 }
 
-syv.tree<-calcSWA(syv.tree, 0.8)
-wcr.tree<-calcSWA(wcr.tree, 0.8)
+syv.tree<-calcSWA(syv.tree)
+wcr.tree<-calcSWA(wcr.tree)
 
 #Pull in radial profile multipliers.
 
-#SYV and WCR_radial are excel-calculated sheets 
+#SYV and WCR_radial are excel-calculated sheets
 #They apply the radial profiles calculated in Radial_calibration.R to 'rings' of sapwood depth
 
 syv.calc.rp<-read.csv('SYV_radial.csv')
 wcr.calc.rp<-read.csv('WCR_radial.csv')
 
-syv.tree$Multiplier<-syv.calc.rp[,24] #Column 24 is the multiplier for actual sapwood area to "effective" sapwood area
-wcr.tree$Multiplier<-wcr.calc.rp[,24] #i.e. where deeper sapwood with lower flux rates counts for less
+syv.tree$MULT_calc<-syv.calc.rp[,24] #Column 24 is the multiplier for actual sapwood area to "effective" sapwood area
+wcr.tree$MULT_calc<-wcr.calc.rp[,24] #i.e. where deeper sapwood with lower flux rates counts for less
 
-#Reorganize sensibly
-col.order<-c(1,2,3,4,5,6,8,7,10)
+
