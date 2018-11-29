6acc0ed95bedaa66ac41f9c758d5f897840adabd
Bethany Blakely <bethanyblakely@bethanys-mbp.dhcp.nd.edu>
2018-11-29 14:22:52 -0500

    AGU poster draft commit. Not neat or clean yet, but generates AGU poster figures for 11-29. Added big phenological section. One unit correction also

--- SapfluxScale_pls_fia.R
+++ SapfluxScale_pls_fia.R
@@ -1,17 +1,16 @@
 
 
-#Check to see if var exists, i.e. if fia.process has already been run
-if (!exists('fia.diam')){ 
-
-source('fia_process.R') #Read in and process FIA data, Script takes ~10min
-
-}
+#Data
+if (!exists('fia.diam')){ source('fia_process.R')} #Read in and process FIA data, Script takes ~10min
 
 fia.comp.raw<-fia.comp
 fia.dens.raw<-fia.dens
 fia.diam.raw<-fia.diam
 
 rm(list=setdiff(ls(), c('fia.comp.raw','fia.dens.raw','fia.diam.raw')))
+
+if(!exists('wcr.master')){source('Prepare_Data.R')} #Read in and process tower and site data, Script takes a few min.
+
 #Read in paleo data
 pls.comp.raw<-read.csv('plss_composition_alb_v0.9-10.csv')
 pls.dens.raw<-read.csv('plss_density_alb_v0.9-10.csv')
@@ -100,7 +99,7 @@ swa.calc<-function(comp,diam,dens){
   #####
   
   #Multiply out across all stems to get total sapwood area
-  dens.stem<-dens.dat*6400
+  dens.stem<-dens.dat*6400  #assumes PLS is in stems per hectare (check!)
   swa.tot<-swa.eachtree*dens.stem*0.75 #I think the .75 is for radial profile
   
   #clip extreme value for plotting
@@ -116,27 +115,28 @@ pls.swa<-swa.calc(pls.comp,pls.diam,pls.dens)
 
 quickplot(rowSums(fia.swa, na.rm=TRUE))
 quickplot(rowSums(pls.swa, na.rm=TRUE))
+#quickplot(rowSums(pls.swa.orig, na.rm=TRUE))
 
 georef<-fia.comp[,1:2]
 
 fluxrates.lut<-read.csv('FLUX_LUT.csv')
 
 #Now transpiration
-transp.calc<-function(swa, dens){  #swa: sapwood calculated above. 
+transp.calc<-function(swa, dens, fluxdf=fluxrates.lut){  #swa: sapwood calculated above. 
   
   #make general references
   georef<-dens[,1:2]
   dimref<-dens[,4:ncol(dens)]
   
   #Read in lit/measured transpiration rates
-  fluxrates.lut<-read.csv('FLUX_LUT.csv')
+  fluxrates.lut<-fluxdf
   
   #make space and name it
   flow.tot<-data.frame(matrix(0, nrow(dimref),ncol(dimref))) #was NA
   colnames(flow.tot)<-colnames(dimref)
   
   #fill with generalizations
-  flow.tot[,]<-swa*fluxrates.lut$FLUX[fluxrates.lut$Taxon=='Hardwood']*86400  #Baseline: all things mean of hardwoods
+  flow.tot[,]<-swa*fluxrates.lut$FLUX[fluxrates.lut$Taxon=='Hardwood']*86400  #Baseline: all things mean of hardwoods. The 86400 is for seconds -> days
   
   EG.sub<-c(12,14,23,25) #fir,hemlock,spruce,tamarack
   flow.tot[,EG.sub]<-swa[,EG.sub]*fluxrates.lut$FLUX[fluxrates.lut$Taxon=='Evergreen']*86400 #Step 2, evergreens mean of evergreens
@@ -164,17 +164,17 @@ transp.calc<-function(swa, dens){  #swa: sapwood calculated above.
 fia.transp<-transp.calc(fia.swa,fia.dens)
 pls.transp<-transp.calc(pls.swa,pls.dens)
 
-fia.transp.tot<-cbind(georef,rowSums(fia.transp[3:31], na.rm=TRUE))
+fia.transp.tot<-cbind(georef,rowSums(fia.transp[3:31], na.rm=TRUE));
 pls.transp.tot<-cbind(georef,rowSums(pls.transp[3:31], na.rm=TRUE))
 
-#plotting
+####plotting####
 library(maps)
 library(raster)
 library(RColorBrewer)
 
 breakset.abs<-c(0,2e8,4e8,6e8,8e8,1e9,1.2e9)
 breakset.diff<-c(-1.2e9,-8e8,-4e8,-2e8,2e8,4e8,8e8,1.2e9)
-plotnice<-function(toplot, breakset, colset='BrBG', title='sapflux'){
+plotnice<-function(toplot, breakset=breakset.abs, colset='BrBG', title='sapflux'){
   
   quants<-quantile(toplot[,3],c(0.03, 0.97), na.rm=TRUE)
 
@@ -190,12 +190,12 @@ plotnice<-function(toplot, breakset, colset='BrBG', title='sapflux'){
 }
 
 par(mar=c(4,4,4,1))
-plotnice(pls.transp.tot/(24*6400*10), breakset.abs/(24*6400*10), title='Historic')
-plotnice(fia.transp.tot/(24*6400*10), breakset.abs/(24*6400*10),title='Modern')
+plotnice(pls.transp.tot/(24*64*10), breakset.abs/(24*64*10), title='Historic')
+plotnice(fia.transp.tot/(24*64*10), breakset.abs/(24*64*10),title='Modern')
 
-#real messy conversion. 24 converts days - > hours, 6400 converts 8km2 -> 1km2
-#10 is a weird one; original numbers multiply m2/s * cm2.
-# m2 | cm2 |  m2     ->   m3
+#real messy conversion. 24 converts days - > hours, 64 converts 8km2 -> 1km2
+#10 is a weird one; original numbers multiply m/s * cm2.
+# m | cm2 |  m2     ->   m3
 # s  |     |10000cm2 -> s * 10000
 #But now we might as well do L  (also kg) so
 #   m3     | 1000 L ->   L
@@ -206,16 +206,15 @@ plotnice(fia.transp.tot/(24*6400*10), breakset.abs/(24*6400*10),title='Modern')
 diff.transp<-cbind(georef,fia.transp$transp-pls.transp$transp)
 diff.transp.pr<-cbind(georef,fia.transp.tot[,3]-pls.transp.tot[,3])
 
-
-
-plotnice(diff.transp/(24*6400*10),breakset.diff/(24*6400*10), colset="RdYlGn", title='Difference (Modern-Historic)')
-plotnice(diff.transp.pr/(24*6400*10),breakset.diff/(24*6400*10), colset="RdYlGn", title='Difference (Modern-Historic)')
+plotnice(diff.transp/(24*64*10),breakset.diff/(24*64*10), colset="RdYlGn", title='Difference (Modern-Historic)')
+plotnice(diff.transp.pr/(24*64*10),breakset.diff/(24*64*10), colset="RdYlGn", title='Difference (Modern-Historic)')
 
 colnames(diff.transp.pr)[3]<-"tdiff"
 write.csv(diff.transp.pr, "pls.fia.transp.diff.csv")
 
 sp.plot='no'  #Do you want regressions by species plots?
 
+#####Individual taxa converstion chart####
 if(sp.plot=='yes'){
 
 pls.pctcomp<-pls.comp[4:32]/rowSums(pls.comp[4:32])
@@ -241,7 +240,6 @@ for(i in 1:29){
 }
 
 }
-
 
 
 pls.sums<-colSums(pls.transp.cols, na.rm=TRUE)
@@ -289,3 +287,169 @@ text(3,-1.4e11, "Lost", font=2)
 
 plot(all$FLUX[order(all$X2)], pch=17,cex=3, ylim=c(0,7e-5), col=all$col[order(all$X2)], xaxt='n', ylab='Flux rate', xlab='')
 box(lwd=3)
+#####
+
+#Now everything again for phenology
+
+wcr.15<-read.csv('WCR_2015_SAPFLUX.csv')[,5:18];syv.15<-read.csv('SYV_2015_SAPFLUX.csv')[,5:24]
+wcr.gap[is.na(wcr.gap)]<-wcr.15[is.na(wcr.gap)]
+syv.gap[is.na(syv.gap)]<-syv.15[is.na(syv.gap)]
+
+syv.gap<-0.000119*((syv.gap/10)^1.21)
+wcr.gap<-0.000119*((wcr.gap/10)^1.21)
+
+####Get monthly transp rates####
+wcr.sap.day<-aggregate(wcr.gap, by=list(wcr.twr.2016$'DOY'), FUN='mean', na.rm=TRUE )
+syv.sap.day<-aggregate(syv.gap, by=list(syv.twr.2016$'DOY'), FUN='mean', na.rm=TRUE )
+
+monthvec<-c(rep(1, 31), rep(2, 29), rep(3, 31), 
+            rep(4, 30), rep(5, 31), rep(6, 30),
+            rep(7, 31), rep(8, 31), rep(9, 30),
+            rep(10, 31), rep(11, 30), rep(12, 31))
+
+wcr.monthagg<-cbind(wcr.sap.day[2:15], monthvec); wcr.month<-aggregate(wcr.monthagg, by=list(monthvec), FUN='mean', na.rm=TRUE)
+syv.monthagg<-cbind(syv.sap.day[2:21], monthvec); syv.month<-aggregate(syv.monthagg, by=list(monthvec), FUN='mean', na.rm=TRUE)
+#####
+
+#Function from calc sapflow full
+summ.flux<-function(source,tree,sapcol){ 
+  sapsum<-data.frame(matrix(nrow=nrow(source), ncol=length(unique(tree$SPP))))
+  for (i in 1:length(unique(tree$SPP))){
+    soi<-as.character(unique(tree$SPP)[i])
+    #sp<-unname(rowMeans(syv.master[,sapstart+which(syv.tree$SPP==soi)], na.rm=TRUE))
+    sapsum[,i]<-unname(rowMeans(source[,sapcol+which(tree$SPP==soi)], na.rm=TRUE))
+  }
+  colnames(sapsum)<-as.character(unique(tree$SPP))
+  sapsum$UK<-rowMeans(sapsum)
+  return(sapsum)
+}
+
+syv.trmonth<-summ.flux(syv.month, syv.tree, 1)
+wcr.trmonth<-summ.flux(wcr.month, wcr.tree, 1)
+
+#Get scalars for other SPP
+
+wcr.norm<-data.frame(apply(wcr.trmonth, 2, function(x) x/max(x, na.rm=TRUE)))
+syv.norm<-data.frame(apply(syv.trmonth, 2, function(x) x/max(x, na.rm=TRUE)))
+
+scalarvec<-rowMeans(cbind(wcr.norm$UK, syv.norm$UK), na.rm=TRUE)
+
+####Build flux lut####
+#Probably a better way to do this...
+acsamean<-rowMeans(cbind(wcr.trmonth$ACSA, syv.trmonth$ACSA), na.rm=TRUE)
+osvimean<-rowMeans(cbind(wcr.trmonth$OSVI, syv.trmonth$OSVI), na.rm=TRUE)
+ukmean<-rowMeans(cbind(wcr.trmonth$UK, syv.trmonth$UK), na.rm=TRUE)
+
+trmonth<-cbind(acsamean, osvimean, wcr.trmonth$TIAM, wcr.trmonth$FRPE, 
+               syv.trmonth$TSCA, syv.trmonth$BEAL)
+colnames(trmonth)<-c('ACSA', 'OSVI','TIAM','FRPE','TSCA','BEAL')
+
+rm('acsamean','osvimean','ukmean')
+#interpolate
+trmonth<-data.frame(na.approx(trmonth))
+#####
+
+#Months
+fia.month.transp<-matrix(data=NA,nrow=nrow(fia.transp.tot), ncol=12) #Months on columns, locations on rows
+pls.month.transp<-matrix(data=NA,nrow=nrow(fia.transp.tot), ncol=12) #Months on columns, locations on rows
+par(mfrow=c(1,2))
+for(i in c(1:12)){
+  
+lut<-fluxrates.lut
+
+lut$FLUX<-round(lut$FLUX*scalarvec[i], digits=7) #All growing season numbers, scaled
+lut$FLUX[match(colnames(trmonth), fluxrates.lut$SPP)]<-round(as.numeric(trmonth[i,]), digits=7) #Add in measured numbers
+lut$FLUX[12]<-mean(lut$FLUX[c(1:4,7:9,11)]);lut$FLUX[13]<-mean(lut$FLUX[c(5:6,10)]) #Average for hardwoods, softwoods
+
+fia.mo<-transp.calc(fia.swa/10000, fia.dens, lut) # The  /10000 converts sapwood area in cm2 to m2.
+pls.mo<-transp.calc(pls.swa/10000, pls.dens, lut) # When m/s of sap flow is multiplied by m2 sapwood area, the output is m3/s. There's also a s>day conversion in original function so,
+#Output is in m3/day for the whole grid cell.
+
+fia.month.transp[,i]<-rowSums(fia.mo[,3:31], na.rm=TRUE)
+pls.month.transp[,i]<-rowSums(pls.mo[,3:31], na.rm=TRUE)
+
+plotguy.fia<-cbind(georef, fia.month.transp[,i])
+plotguy.pls<-cbind(georef, pls.month.transp[,i])
+
+plotnice(plotguy.pls, breakset=breakset.abs*8e-4, title='Historical')
+plotnice(plotguy.fia, breakset=breakset.abs*8e-4,title='Modern')
+#COOL
+}
+par(mfrow=c(1,1))
+transpdiff<-(fia.month.transp-pls.month.transp)  #Converts m3/day to m3/s, then m3 per second to L/s, then divides by grid cell size (640000000 m2 per 8km2) 
+
+convfactor<-1000/(86400*64000000) #start: m3/day 8km ; 1000 L/m3, 86400 seconds/day ; 64000000 m2/8km grid cell
+transpdiff.unit<-transpdiff*convfactor
+
+write.csv(transpdiff.unit,"/Users/bethanyblakely/Desktop/Analysis/MIP/TranspChg_Sap.csv")
+
+#Subsetting to transitions
+
+egtaxa<-c(8,12,14,21,23)
+hwtaxa<-c(1:7,9:10,13,15:17,19:20,22,24:26,28:29)
+
+#comp based
+
+fia.dat<-fia.comp[,4:32]
+fia.eg<-rowSums(fia.dat[egtaxa]);fia.dc<-rowSums(fia.dat[hwtaxa])
+fia.total<-rowSums(fia.dat[,c(1:17,18:26,28:29)])
+
+#fia.egdom<-which((fia.eg/fia.total)>0.6)
+fia.dcdom<-which((fia.dc/fia.total)>0.6)
+
+pls.dat<-pls.comp[,4:32]
+pls.eg<-rowSums(pls.dat[egtaxa]);pls.dc<-rowSums(pls.dat[hwtaxa])
+pls.total<-rowSums(pls.dat[,c(1:17,18:26,28:29)])
+
+#pls.egdom<-which((pls.eg/pls.total)>0.6)
+#pls.dcdom<-which((pls.dc/pls.total)>0.6)
+
+transitions<-which(((pls.eg/pls.total)>=0.6 & (fia.dc/fia.total)>=0.6) | #EG to DC
+                     ((pls.eg/pls.total>=0.4 & (pls.eg/pls.total<0.6)) & (fia.dc/fia.total)>=0.6) | #MX to DC
+                      pls.eg/pls.total>=0.6 & ((fia.dc/fia.total>=0.4 & fia.dc/fia.total<0.6))) #EG to MX
+
+evelse<-which(((pls.eg/pls.total)<0.4 | (fia.dc/fia.total)<0.4) | #doesn't start or end mixed / dominant
+                ((pls.eg/pls.total)>=0.4 & (fia.dc/fia.total)<0.6)) #starts mixed but doesnt go deciduous
+              
+length(transitions)
+
+write.csv(transitions,"/Users/bethanyblakely/Desktop/Analysis/MIP/EGtoDC_ind.csv")
+
+
+transp.dcify<-transpdiff.unit[transitions,]
+
+transp.dcify.plot<-transpdiff.unit; transp.dcify.plot[evelse,]<-NA
+
+
+plot(colMeans(transp.dcify), type='l', lwd=3, ylim=c(-1e-5,2e-5))
+for(i in 1:nrow(transp.dcify)){
+  lines(transp.dcify[i,], col='gray', lwd=0.5)
+  lines(colMeans(transp.dcify), lwd=2)
+}
+
+#A map
+par(mfrow=c(1,1))
+
+
+background<-rasterFromXYZ(cbind(pls.comp.raw[1:2], rowSums(pls.comp.raw[4:32])))
+plot(background, col='black', legend=FALSE, main='Transpiration Change')
+
+default<-rasterFromXYZ(pls.transp.tot)
+rasterFromXYZ(cbind(pls.comp.raw[1:2], rowSums(pls.comp.raw[,4:32])))
+#plot(default, add=TRUE, col='black')
+
+breaknum=11
+col<-colorRampPalette(c('red','light gray', 'blue'))
+breaks<-seq(from=-4e-4, to=4e-4, length.out=breaknum)
+
+transprast<-rasterFromXYZ(cbind(georef,transp.dcify.plot[,7]))
+plot(transprast, add=TRUE, breaks=breaks, col=col(breaknum))
+
+#mask<-default
+#mask[transitions]<-NA
+#plot(mask, add=T, col='black')
+
+
+
+
+
